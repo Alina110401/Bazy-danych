71e539239f054a9244474a578fa3a95f106460ab
Alina Slabko <alina.slabko@student.put.poznan.pl>
2025-05-15 19:54:09 +0200

    Dodaj pliki projektów.

---
+++ Bazy danych/Form1.Designer.cs
@@ -0,0 +1,370 @@
+﻿namespace Bazy_danych
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            label14 = new Label();
+            label15 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            textBox9 = new TextBox();
+            textBox10 = new TextBox();
+            textBox11 = new TextBox();
+            textBox12 = new TextBox();
+            textBox13 = new TextBox();
+            comboBox1 = new ComboBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(451, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(117, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Poznań, dnia ";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(574, 23);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(150, 31);
+            textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(152, 144);
+            label2.Name = "label2";
+            label2.Size = new Size(128, 25);
+            label2.TabIndex = 2;
+            label2.Text = "numer albumu";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(152, 212);
+            label3.Name = "label3";
+            label3.Size = new Size(131, 25);
+            label3.TabIndex = 3;
+            label3.Text = "nazwisko i imię";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(164, 280);
+            label4.Name = "label4";
+            label4.Size = new Size(109, 25);
+            label4.TabIndex = 4;
+            label4.Text = "semestr, rok";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(53, 347);
+            label5.Name = "label5";
+            label5.Size = new Size(224, 25);
+            label5.TabIndex = 5;
+            label5.Text = " kierunek i stopień studiów";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(32, 564);
+            label6.Name = "label6";
+            label6.Size = new Size(647, 25);
+            label6.TabIndex = 6;
+            label6.Text = "Proszę o umożliwienie mi przystąpienia do egzaminu komisyjnego z przedmiotu";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(32, 674);
+            label7.Name = "label7";
+            label7.Size = new Size(200, 25);
+            label7.TabIndex = 7;
+            label7.Text = "prowadzonego przez    ";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(32, 733);
+            label8.Name = "label8";
+            label8.Size = new Size(194, 25);
+            label8.TabIndex = 8;
+            label8.Text = "Uzasadnienie wniosku: ";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(439, 617);
+            label9.Name = "label9";
+            label9.Size = new Size(67, 25);
+            label9.TabIndex = 9;
+            label9.Text = "punkty";
+            label9.Click += label9_Click;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(291, 393);
+            label10.Name = "label10";
+            label10.Size = new Size(497, 50);
+            label10.TabIndex = 10;
+            label10.Text = "\r\nPRODZIEKAN WYDZIAŁU  INFORMATYKI I TELEKOMUNIKACJI";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(167, 509);
+            label11.Name = "label11";
+            label11.Size = new Size(492, 25);
+            label11.TabIndex = 11;
+            label11.Text = "WNIOSEK O PRZEPROWADZENIE EGZAMINU KOMISYJNEGO";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(547, 828);
+            label12.Name = "label12";
+            label12.Size = new Size(191, 25);
+            label12.TabIndex = 12;
+            label12.Text = "data i podpis studenta";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(337, 864);
+            label13.Name = "label13";
+            label13.Size = new Size(84, 25);
+            label13.TabIndex = 13;
+            label13.Text = "DECYZJA";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Location = new Point(262, 912);
+            label14.Name = "label14";
+            label14.Size = new Size(439, 25);
+            label14.TabIndex = 14;
+            label14.Text = " zgod(ę)y na przeprowadzenie egzaminu komisyjnego";
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Location = new Point(62, 961);
+            label15.Name = "label15";
+            label15.Size = new Size(121, 25);
+            label15.TabIndex = 15;
+            label15.Text = "Skład komisji:";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(286, 138);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(150, 31);
+            textBox2.TabIndex = 16;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(289, 212);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(150, 31);
+            textBox3.TabIndex = 17;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(289, 275);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(150, 31);
+            textBox4.TabIndex = 18;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(286, 341);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(150, 31);
+            textBox5.TabIndex = 19;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(32, 611);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(374, 31);
+            textBox6.TabIndex = 20;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(538, 614);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(219, 31);
+            textBox7.TabIndex = 21;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(216, 674);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(190, 31);
+            textBox8.TabIndex = 22;
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(216, 733);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(541, 31);
+            textBox9.TabIndex = 23;
+            // 
+            // textBox10
+            // 
+            textBox10.Location = new Point(535, 794);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(219, 31);
+            textBox10.TabIndex = 24;
+            // 
+            // textBox11
+            // 
+            textBox11.Location = new Point(62, 1000);
+            textBox11.Name = "textBox11";
+            textBox11.Size = new Size(236, 31);
+            textBox11.TabIndex = 25;
+            // 
+            // textBox12
+            // 
+            textBox12.Location = new Point(62, 1051);
+            textBox12.Name = "textBox12";
+            textBox12.Size = new Size(236, 31);
+            textBox12.TabIndex = 26;
+            // 
+            // textBox13
+            // 
+            textBox13.Location = new Point(62, 1111);
+            textBox13.Name = "textBox13";
+            textBox13.Size = new Size(236, 31);
+            textBox13.TabIndex = 27;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Wyrażam", "Nie wyrażam" });
+            comboBox1.Location = new Point(62, 909);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(182, 33);
+            comboBox1.TabIndex = 28;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(839, 1170);
+            Controls.Add(comboBox1);
+            Controls.Add(textBox13);
+            Controls.Add(textBox12);
+            Controls.Add(textBox11);
+            Controls.Add(textBox10);
+            Controls.Add(textBox9);
+            Controls.Add(textBox8);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(label15);
+            Controls.Add(label14);
+            Controls.Add(label13);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private Label label13;
+        private Label label14;
+        private Label label15;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private TextBox textBox9;
+        private TextBox textBox10;
+        private TextBox textBox11;
+        private TextBox textBox12;
+        private TextBox textBox13;
+        private ComboBox comboBox1;
+    }
+}
---
+++ Bazy danych/Form1.cs
@@ -0,0 +1,15 @@
+namespace Bazy_danych
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
